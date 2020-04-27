2ed821193b790b025d628df95f4c5d648afcb40a
Luis Castillo <lcastillo770@gmail.com>
2020-04-27 23:49:59 +0200

    I fixed some typo

--- src/sorting/MergeSortClass.java
+++ src/sorting/MergeSortClass.java
@@ -2,25 +2,25 @@ package sorting;
 
 public class MergeSortClass {
 	//testing
-	public void mergeSort(int[] a, int lenght) {
-		if (lenght < 2) {
+	public void mergeSort(int[] a, int length) {
+		if (length < 2) {
 			return;
 		}
 
-		int middle = lenght / 2;
+		int middle = length / 2;
 		int[] l = new int[middle];
-		int[] r = new int[lenght - middle];
+		int[] r = new int[length - middle];
 
 		for (int i = 0; i < middle; i++) {
 			l[i] = a[i];
 		}
-		for (int i = middle; i < lenght; i++) {
+		for (int i = middle; i < length; i++) {
 			r[i - middle] = a[i];
 		}
 		mergeSort(l, middle);
-		mergeSort(r, lenght - middle);
+		mergeSort(r, length - middle);
 
-		merge(a, l, r, middle, lenght - middle);
+		merge(a, l, r, middle, length - middle);
 	}
 
 	public void merge(int[] a, int[] l, int[] r, int left, int right) {
